6123a3e8ada9d0a8373164e0001e52f3b8744ec1
Guilherme Braga <crazyboysfh3@gmail.com>
2024-03-10 21:50:52 -0300

    finished something

---
+++ js/characterCreator.js
@@ -0,0 +1,8 @@
+const card = document.getElementById('card');
+const cards = document.getElementById('cards');
+
+card.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    
+})
